7d3e5ce754bb1454b579a395b42dac3c6c060bd1
hirushan2001 <135137760+hirushan2001@users.noreply.github.com>
2024-07-21 13:18:59 +0530

    FM | update animation in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,14 +9,14 @@ const Home = () => {
     <div className="home-container" id="home">
       <main className="main-content">
         <div className="left-content">
-          <p className="job-title">Software Developer</p>
-          <h1 className="greeting">Hello I'm<br /><span className="name">Dhananjana Hirushan</span></h1>
-          <p className="description">
+          <p className="job-title animate-text">Software Developer</p>
+          <h1 className="greeting animate-text">Hello I'm<br /><span className="name animate-text">Dhananjana Hirushan</span></h1>
+          <p className="description animate-text">
             I excel at crafting elegant digital experiences and<br />
             I am proficient in various programming languages and technologies.
           </p>
-          <Button icon={<DownloadOutlined />} className="download-cv-btn">DOWNLOAD CV</Button>
-          <div className="social-links">
+          <Button icon={<DownloadOutlined />} className="download-cv-btn animate-button">DOWNLOAD CV</Button>
+          <div className="social-links animate-text">
             <a href="#github"><GithubOutlined /></a>
             <a href="#linkedin"><LinkedinOutlined /></a>
             <a href="#mail"><MailOutlined /></a>
@@ -25,7 +25,7 @@ const Home = () => {
         </div>
         <div className="right-content">
           <div className="profile-image-container">
-            <img src={img} alt="Dhananjana Hirushan" className="profile-image" />
+            <img src={img} alt="Dhananjana Hirushan" className="profile-image animate-image" />
           </div>
         </div>
       </main>
